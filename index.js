cdd3208d9c3e1ca12c319cb109f1e53f081b35d0
aavashkarmacharya <karmacharya.aavash0@gmail.com>
2025-11-26 14:43:24 +0545

    array and object practice

--- index.js
+++ index.js
@@ -3,42 +3,35 @@ let y = 11;
 const z = 4;
 let name = "chatur";
 function compare(a, b) {
-    if (a < b) {
-        console.log(`${b} is the  largest number `);
-    }
-    else {
-        console.log(`${a} is the largest number`);
-    }
+  if (a < b) {
+    console.log(`${b} is the  largest number `);
+  } else {
+    console.log(`${a} is the largest number`);
+  }
 }
 compare(5, 5);
 
-
 function mult(a, b) {
-    return a * b;
+  return a * b;
 }
 console.log(mult(5, 7));
 
-
-
 function call(name, callback) {
-    console.log(`hello ${name}`);
-    callback();
+  console.log(`hello ${name}`);
+  callback();
 }
 
-
 function rancho() {
-    console.log("september 5 ");
+  console.log("september 5 ");
 }
 call(name, rancho);
 
-
 function add() {
-    let r;
-    r = x + y;
-    return r;
+  let r;
+  r = x + y;
+  return r;
 }
 console.log(add());
-
 
 let arr = [1, 2, 3, 4];
 let i = 0;
@@ -51,17 +44,15 @@ arr.shift();
 console.log(arr.includes(3));
 
 for (i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+  console.log(arr[i]);
 }
-
 
 let user = { gen: "pilot", age: 11 };
 console.log(`Whats up bro u a ${user.gen} and your age is ${user.age}`);
 
-
 function test(arrowcallback) {
-    console.log("this is a test function for arrow bs");
-    arrowcallback();
+  console.log("this is a test function for arrow bs");
+  arrowcallback();
 }
 let typeno = () => console.log("OMG MIRACLEEE!!!");
 test(typeno);
@@ -71,20 +62,43 @@ console.log(divide(100, 5));
 
 console.log("hello bro");
 
-
 let fruits = ["Apple", "banana", "pineapple"];
 fruits.push("Pumpkin is not a fruittt");
 fruits.unshift("Pumpkin is probably a fruit");
 
+const person = [
+  {
+    id: 1,
+    name: "ram",
+    isMarried: true,
+    cars: ["bmw", "toyota", "mercedes"],
+  },
+  {
+    id: 2,
+    name: "rita",
+    isMarried: true,
+    cars: ["bmw", "toyota", "mercedes"],
+  },
+  {
+    id: 3,
+    name: "sita",
+    isMarried: true,
+    cars: ["bmw", "toyota", "mercedes"],
+  },
+];
 
+person.forEach((user) => {
+  console.log(user.name);
+});
 
+const filteredData = person.filter((user) => {
+  if (user.id === 3) {
+    return user;
+  }
+});
 
-for(i=0; i<fruits.length; i++){
-console.log(fruits[i]);
+console.log(filteredData)
+
+for (i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
 }
-
-
-
-
-
-
---
+++ todo.js
@@ -0,0 +1,83 @@
+const user = [
+    {
+        name: "ram",
+        age: 12
+    },
+    {
+        name: "sita",
+        age: 15
+    },
+
+    {
+        name: "gita",
+        age: 15
+    },
+    {
+        name: "nita",
+        age: 15
+    },
+    {
+        name: null,
+        age: 18,
+    }
+]   
+//initializing a default value for name
+ for(person of user){
+let yy= "guest";
+    person.name = person.name??yy;
+    console.log(person.name);
+
+} 
+
+//filtering the age by 15
+const filtered = user.filter(person => person.age === 15 );
+console.log(filtered);
+
+//printing names from array
+const names = user.map(person => person.name);
+console.log(names);
+
+//adding thapa to every person
+let y = user.map(person => person.name + " Thapa");
+console.log(y);
+
+//random for default initialization 
+let r= "nita";
+let z = (name) => {
+    name = name?? r;
+console.log(name);
+}
+z("hariio");
+
+
+/*for( let details of user){
+    const check1 = (details) =>{
+        if(details.age !== 18){
+    console.log(`Hey!, ${details.name} you are ${details.age} years old  `);
+}
+    }
+check1(details);
+}
+*/
+//checking conditions w/o filter
+ user.forEach(details =>{
+    if(details.age != 15){
+        console.log(`Hey!, ${details.name} you are ${details.age} years old `);
+    }
+ } )
+ //checking with filter
+
+ let b = user.filter(person => person.age === 15);
+ console.log(b);
+
+//practice ig
+ let  updating = user.map(details => details.name + "_Thapa");
+ console.log(updating);
+
+
+
+
+ 
+
+
+
